e716b5bf58860a0aa3b36ae89f47c551f612a1db
Emma Prené <emma.prene@gmail.com>
2020-05-10 17:12:26 +0200

    done

--- server.js
+++ server.js
@@ -1,6 +1,58 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/books'
+mongoose.connect(mongoUrl, { userNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Author = mongoose.model('Author', {
+  name: String,
+})
+
+
+const Book = mongoose.model('Book', {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    //vilken den relaterar till
+    ref: 'Author',
+  }
+})
+
+
+
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    //annars skapas nya författare hela tiden, detta gör att det blir tomt varje gång funktionen körs och sen fylls på igen
+    await Author.deleteMany()
+    await Book.deleteMany()
+
+    const tolkien = new Author({ name: 'J.R.R Tolkien' })
+    await tolkien.save()
+
+    const rowling = new Author({ name: 'JK Rowling' })
+    await rowling.save()
+
+
+    await new Book({
+      title: 'Harry Potter 1',
+      author: rowling,
+    }).save()
+    await new Book({ title: 'Harry Potter 2', author: rowling, }).save()
+    await new Book({ title: 'Harry Potter 3', author: rowling, }).save()
+    await new Book({ title: 'Harry Potter 4', author: rowling, }).save()
+    await new Book({ title: 'Harry Potter 5', author: rowling, }).save()
+    await new Book({ title: 'Harry Potter 6', author: rowling, }).save()
+    await new Book({ title: 'Harry Potter 7', author: rowling, }).save()
+    await new Book({
+      title: 'The Hobbit',
+      author: tolkien,
+    }).save()
+  }
+  seedDatabase()
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -17,6 +69,38 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+app.get('/authors', async (req, res) => {
+  const authors = await Author.find()
+  res.json(authors)
+})
+
+app.get('authors/:id', async (erq, res) => {
+  const author = await Author.findById(req.params.id)
+  if (author) {
+    res.json(author)
+  } else {
+    res.status(404).json({ error: 'Author not found' })
+  }
+})
+
+app.get('/authors/:id/books', async (req, res) => {
+  const author = await Author.findById(req.params.id)
+  if (author) {
+    const books = await Book.find({ author: mongoose.Types.ObejctId(author.id) })
+    res.json(books)
+  } else {
+    res.status(404).json({ error: 'Author not found' })
+  }
+}
+)
+
+app.get('/books', async (req, res) => {
+  const books = await Book.find().populate('author')
+  res.json(books)
+})
+
+
 
 // Start the server
 app.listen(port, () => {
